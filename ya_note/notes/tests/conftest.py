8b20643b95ebb2ec829ec923d3899808af93e2b2
Elena Gruzintseva <elenagruzinceva@yandex.ru>
2024-09-28 22:22:29 +0300

    unittest correct

--- ya_note/notes/tests/conftest.py
+++ ya_note/notes/tests/conftest.py
@@ -43,9 +43,3 @@ class TestBase(TestCase):
             'slug': 'Zametkaa',
             'author': cls.author
         }
-
-        FIELD_NAMES = ('title', 'text', 'slug', 'author')
-        FIELD_DATA = ('Заголовок', 'Текст заметки', SLUG)
-
-        cls.form_data = dict(zip(FIELD_NAMES, FIELD_DATA))
-        cls.field_data = (*FIELD_DATA, cls.author)
--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,5 +1,4 @@
 from http import HTTPStatus
-import unittest
 
 from pytils.translit import slugify
 
@@ -12,63 +11,56 @@ from .conftest import (
 
 class CreatedNote(TestBase):
 
-    def method_created_note(self):
-        notes = set(Note.objects.all())
-        self.assertRedirects(
-            self.author_client.post(URL_NOTES_ADD, data=self.new_created_note),
-            URL_NOTES_SUCCESS)
-        difference = set(Note.objects.all()) - notes
-        self.assertEqual(len(difference), 1)
-        note = difference.pop()
-        self.assertEqual(note.title, self.new_created_note['title'])
-        self.assertEqual(note.author, self.author)
-        self.assertEqual(note.slug, self.new_created_note['slug'])
-        self.assertEqual(note.text, self.new_created_note['text'])
+    def method_created_note(self, field_data):
+        note = Note.objects.get(title=field_data['title'])
+        data = dict(field_data)
+        for key, value in data.items():
+            with self.subTest(key=key, value=value):
+                self.assertEqual(note.title, data['title'])
+                self.assertEqual(note.text, data['text'])
+                self.assertEqual(note.slug, data['slug'])
+                self.assertEqual(note.author, self.author)
+
+    def equal(self, expected_count):
+        notes_count = Note.objects.count()
+        self.assertEqual(expected_count, notes_count)
 
 
 class TestLogic(CreatedNote):
 
     def test_user_can_create_note(self):
-        print(f'НАШ ТЕСТ ТУТ!')
-        super().method_created_note()
-
-    @unittest.skip(reason='Перенесите алгоритм: запрос, извлечение созданного, контроль полей - в новый метод.')
-    def test_user_can_create_note_without_slug(self):
-        super().method_created_note()
+        expected_count = Note.objects.count() + 1
         self.assertRedirects(
             self.author_client.post(URL_NOTES_ADD, data=self.new_created_note),
             URL_NOTES_SUCCESS)
-        new_note2 = Note.objects.get(self.new_created_note)
-        new_note2.pop('slug')
+        super().equal(expected_count)
+        super().method_created_note(self.new_created_note)
 
-        self.assertEqual(slugify(new_note2['title']), new_note2.slug)
+    def test_user_can_create_note_without_slug(self):
+        expected_count = Note.objects.count() + 1
+        self.new_created_note.pop('slug')
+        self.assertRedirects(
+            self.author_client.post(URL_NOTES_ADD, data=self.new_created_note),
+            URL_NOTES_SUCCESS)
+        super().equal(expected_count)
+        new_created_note = Note.objects.get(
+            title=self.new_created_note['title']
+        )
+        self.assertEqual(
+            new_created_note.slug, slugify(self.new_created_note['title'])
+        )
 
-        # response = self.author_client.post(
-        #     URL_NOTES_ADD, data=self.new_created_note
-        # )
-        # self.assertRedirects(response, URL_NOTES_SUCCESS)
-        # created_note = Note.objects.get(title=self.new_created_note['title'])
-        # self.assertEqual(
-        #     created_note.slug, slugify(self.new_created_note['title'])
-        # )
-        # self.assertEqual(created_note.title, self.new_created_note['title'])
-        # self.assertEqual(created_note.text, self.new_created_note['text'])
-        # self.assertEqual(created_note.author, self.author)
-
-    @unittest.skip(reason='')
     def test_anonymous_user_cant_create_note(self):
-        notes = set(Note.objects.all())
+        expected_count = Note.objects.count()
         self.client.post(URL_NOTES_ADD, data=self.new_created_note)
-        self.assertEqual(notes, set(Note.objects.all()))
+        super().equal(expected_count)
 
-    @unittest.skip(reason='')
     def test_cant_create_note_with_duplicate_slug(self):
-        notes = set(Note.objects.all())
+        expected_count = Note.objects.count()
         self.new_created_note['slug'] = self.note.slug
         self.author_client.post(URL_NOTES_ADD, data=self.new_created_note)
-        self.assertEqual(set(Note.objects.all()), notes)
+        super().equal(expected_count)
 
-    @unittest.skip(reason='')
     def test_author_can_edit_note(self):
         self.assertEqual(
             self.author_client.post(
@@ -81,7 +73,6 @@ class TestLogic(CreatedNote):
         self.assertEqual(note.slug, self.new_created_note['slug'])
         self.assertEqual(note.author, self.note.author)
 
-    @unittest.skip(reason='')
     def test_user_cant_edit_note_of_another_user(self):
         self.assertEqual(self.reader_client.post(
             URL_NOTES_EDIT, data=self.new_created_note
@@ -92,21 +83,19 @@ class TestLogic(CreatedNote):
         self.assertEqual(self.note.text, note.text)
         self.assertEqual(self.note.slug, note.slug)
 
-    @unittest.skip(reason='')
     def test_author_can_delete_note(self):
-        start_notes_count = Note.objects.count()
+        expected_count = Note.objects.count() - 1
         self.author_client.delete(URL_NOTES_DELETE)
-        self.assertEqual(start_notes_count, Note.objects.count() + 1)
+        super().equal(expected_count)
         self.assertFalse(Note.objects.filter(id=self.note.id).exists())
 
-    @unittest.skip(reason='')
     def test_user_cant_delete_note_of_another_user(self):
-        start_notes_count = Note.objects.count()
+        expected_count = Note.objects.count()
         self.assertEqual(
             self.reader_client.delete(
                 URL_NOTES_DELETE).status_code, HTTPStatus.NOT_FOUND
         )
-        self.assertEqual(start_notes_count, Note.objects.count())
+        super().equal(expected_count)
         self.assertIsInstance(self.note, Note)
         note = Note.objects.get(id=self.note.id)
         self.assertEqual(self.note.author, note.author)
